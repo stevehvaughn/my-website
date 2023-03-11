14ccc4997e1854ccbed7948da2c1085fff92cbeb
Steve Vaughn <steve.vaughn@milespartnership.com>
2023-03-10 20:03:09 -0700

    basic nav setup

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,5 +1,15 @@
+import React from "react";
+import Link from 'next/link';
+
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <ul>
+      <li>
+        <Link href="/">Home</Link>
+      </li>
+      <li>
+        <Link href="/about">About Me</Link>
+      </li>
+    </ul>
   )
-}
+} 
