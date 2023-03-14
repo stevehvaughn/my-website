19bd0e19e4279d2b6ac39cc787b7a35d518ea0ed
Steve Vaughn <steve.vaughn@milespartnership.com>
2023-03-14 13:39:30 -0600

    added global fonts open-sans and bitter

---
+++ src/pages/fonts.js
@@ -0,0 +1,12 @@
+import { Open_Sans } from 'next/font/google'
+import { Bitter } from 'next/font/google'
+
+export const open_sans = Open_Sans({
+  subsets: ['latin'],
+  variable: '--font-open-sans'
+})
+
+export const bitter = Bitter({
+  subsets: ['latin'],
+  variable: '--font-bitter'
+})
