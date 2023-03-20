1f726811d00808f4b08681ab5341fbeded651fc8
Steve Vaughn <steve.vaughn@milespartnership.com>
2023-03-20 15:21:10 -0600

    layout adjustment readability

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,9 +4,7 @@ import Footer from './Footer';
 export default function Layout({ children, criteria }) {
   return (
     <>
-      <Navbar
-        criteria={criteria}
-      />
+      <Navbar criteria={criteria} />
       <main>{children}</main>
       <Footer />
     </>
