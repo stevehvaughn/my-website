e2a0263c759361f4b3dfaef36444ebe190eebac1
Steve Vaughn <steve.vaughn@milespartnership.com>
2023-03-30 15:06:59 -0600

    work on performances page and data

--- src/pages/music/perform.js
+++ src/pages/music/perform.js
@@ -1,9 +1,49 @@
 import Layout from "@components/layout"
+import { getAllPerformances } from '@utils/performances';
+import Date from "@components/date";
 
-export default function perform() {
+export default function perform({ allPerformances }) {
   return (
     <Layout criteria='music'>
-      <div>Performances</div>
+      <h1>Upcoming Performances</h1>
+      <div>
+        {allPerformances.map(performance => (
+          <article>
+            <header>
+              <h2>{performance.title}</h2>
+              <h5>
+                {performance.dates.map(item => (
+                  <Date dateString={item} />
+                ))}
+              </h5>
+            </header>
+            <div>
+              <p>{performance.venue.name}</p>
+              <p>{performance.venue.address}</p>
+            </div>
+            <div>
+              <span>with the </span>
+              <span>{performance.ensemble.name}</span>
+            </div>
+            <div>
+              <h5>Repertoire:</h5>
+              {performance.repertoire.map(piece => (
+                <div>{piece.composition} by {piece.composer}</div>
+              ))}
+            </div>
+          </article>
+        ))}
+      </div>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPerformances = getAllPerformances();
+
+  return {
+    props: {
+      allPerformances
+    }
+  }
 }
---
+++ src/utils/venues.js
@@ -0,0 +1,15 @@
+export const venues =
+{
+  vonBraunCenter: {
+    name: "Von Braun Center",
+    address: "700 Monroe Street, Huntsville, Alabama 35801"
+  },
+  lincolnCenter: {
+    name: "Lincoln Center",
+    address: "417 West Magnolia Street, Fort Collins, CO 80521"
+  },
+  timberline: {
+    name: "Timberline Church",
+    address: "2908 S Timberline Rd, Fort Collins, CO 80525"
+  }
+}
