c4fbdb92c4be076c8038783f0139d5e260f3b424
Steve Vaughn <steve.h.vaughn@gmail.com>
2025-07-01 16:07:31 -0400

    fix social logos

--- src/components/Molecules/Socials/Socials.js
+++ src/components/Molecules/Socials/Socials.js
@@ -1,5 +1,5 @@
 import React from "react"
-import * as ReactIcons from 'react-icons/gr'
+import * as ReactIcons from 'react-icons/bi'
 import { socialLinks } from "@utils/data"
 import styles from "./Socials.module.scss"
 
@@ -9,7 +9,7 @@ export default function Socials() {
       <div className={styles.social_header}>Follow me <strong>@stevehvaughn</strong> on all socials!</div>
       <div className={styles.social_links_wrapper}>
         {socialLinks.map(link => {
-          let iconName = "Gr" + link.name;
+          let iconName = "BiLogo" + link.name;
           return (
             <a className={styles.social_link} key={link.name} href={link.url} target="_blank" >{React.createElement(ReactIcons[`${iconName}`], { size: 30 })}</a>
           )
